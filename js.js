c702b672eff8b64eaded86bf71e7fc9815f0d901
iLove3D <artik62@icloud.com>
2026-05-11 16:36:28 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert("If it's later than 10 p.m. and you think it's still a great idea, don't do it 😉")
+	alert("Писать мне после 22.00 не лучшая идея")
 })
